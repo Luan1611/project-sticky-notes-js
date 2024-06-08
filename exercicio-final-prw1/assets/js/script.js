8be76e645bb2fb8451ba7e5b5e1c1060f79a0c9f
Luan1611 <luan_1611@hotmail.com>
2024-06-08 17:26:07 -0300

    anything

--- exercicio-final-prw1/assets/js/script.js
+++ exercicio-final-prw1/assets/js/script.js
@@ -1,24 +1,19 @@
 function checarLogin() {
-    const loginDiv = document.querySelector(".telalogin")
+    const loginDiv = document.querySelector(".inputLogin")
     const sticknotesDiv = document.querySelector(".lembretes");
     const login = localStorage.getItem("tokenJWT");
 
-    if(login){
+    if(login) {
         loginDiv.style.display = "none";
         sticknotesDiv.style.display = "block";
-    }else{
+    } else{
         loginDiv.style.display = "block";
         sticknotesDiv.style.display = "none";
     }
 }
 
-function fazerLogin() {
-    const loginForm = document.querySelector("#login")
-    loginForm.addEventListener("submit", login);
-
-}
-
 function login(e) {
+
     e.preventDefault();
     const email = document.querySelector("#email")
     localStorage.setItem("tokenJWT", email.value);
@@ -26,6 +21,42 @@ function login(e) {
     checarLogin();
 }
 
+async function cadastrar(e) {
+
+    e.preventDefault();
+
+    let botaoClicado = e.target;
+    let valorEmailPessoa = botaoClicado.parentElement.children[0].innerHTML;
+    let valorSenhaPessoa = botaoClicado.parentElement.children[1].innerHTML;
+    let options = {
+        method: "POST",
+        body: JSON.stringify({
+            login: emailPessoa,
+            senha: valorSenhaPessoa
+        }),
+    }
+    const resposta = await fetch("url", options).json();
+
+    console.log(resposta)
+}
+
+const urlLogin = https://ifsp.ddns.net/webservices/lembretes/;
+const tokenJWT = localStorage.getItem("tokenJWT");
+
 checarLogin();
 
-fazerLogin();
+const botaoLogin = document.querySelector("#fazerLogin")
+botaoLogin.addEventListener("click", login);
+
+const botaoCadastro = document.querySelector("#fazerCadastro");
+botaoCadastro.addEventListener("click", cadastrar);
+
+
+
+
+
+
+/* let op = {
+    headers: { "Authorization": `Bearer ${tokenJWT}` }
+    }
+    const res = await fetch("url", op); */

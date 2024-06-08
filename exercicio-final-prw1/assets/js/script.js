3b0d618c8c31cc76493674a8af645e9f39dd4d97
Luan1611 <luan_1611@hotmail.com>
2024-06-08 15:23:15 -0300

    my commit

--- exercicio-final-prw1/assets/js/script.js
+++ exercicio-final-prw1/assets/js/script.js
@@ -1,7 +1,7 @@
-function checarLogin(){
+function checarLogin() {
     const loginDiv = document.querySelector(".telalogin")
-    const sticknotesDiv = document.querySelector(".principal");
-    const login = localStorage.getItem("tokenJWT")
+    const sticknotesDiv = document.querySelector(".lembretes");
+    const login = localStorage.getItem("tokenJWT");
 
     if(login){
         loginDiv.style.display = "none";
@@ -12,13 +12,13 @@ function checarLogin(){
     }
 }
 
-function fazerLogin(){
+function fazerLogin() {
     const loginForm = document.querySelector("#login")
     loginForm.addEventListener("submit", login);
 
 }
 
-function login(e){
+function login(e) {
     e.preventDefault();
     const email = document.querySelector("#email")
     localStorage.setItem("tokenJWT", email.value);
@@ -26,5 +26,6 @@ function login(e){
     checarLogin();
 }
 
-
 checarLogin();
+
+fazerLogin();

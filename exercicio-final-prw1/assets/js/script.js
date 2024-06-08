b4f989c403f57b26f19592f0f9dd40c122bb45d2
Luana Monteiro <luxo11111@gmail.com>
2024-06-08 15:15:20 -0300

    tentativa de apagar e aparecer divs na tela

--- exercicio-final-prw1/assets/js/script.js
+++ exercicio-final-prw1/assets/js/script.js
@@ -1,3 +1,30 @@
-import { checarLogin } from "./functions/Login";
+function checarLogin(){
+    const loginDiv = document.querySelector(".telalogin")
+    const sticknotesDiv = document.querySelector(".principal");
+    const login = localStorage.getItem("tokenJWT")
+
+    if(login){
+        loginDiv.style.display = "none";
+        sticknotesDiv.style.display = "block";
+    }else{
+        loginDiv.style.display = "block";
+        sticknotesDiv.style.display = "none";
+    }
+}
+
+function fazerLogin(){
+    const loginForm = document.querySelector("#login")
+    loginForm.addEventListener("submit", login);
+
+}
+
+function login(e){
+    e.preventDefault();
+    const email = document.querySelector("#email")
+    localStorage.setItem("tokenJWT", email.value);
+    email.value = "";
+    checarLogin();
+}
+
 
 checarLogin();
